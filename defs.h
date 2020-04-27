653e3c3760694237a75f6fda20f6f62ae8742858
Pedro Vitor Piassa <pvpiassa@gmail.com>
2020-04-27 19:34:53 -0300

    Moved global definitions

---
+++ defs.h
@@ -0,0 +1,12 @@
+#ifndef DEFS_H
+#define DEFS_H
+
+#define MAX_PATH 260
+
+const char r_chars[] = {'<', '>', ':', '"', '/', '\\', '|', '?', '*'};
+const char *r_names[] = {"CON", "PRN", "AUX", "NUL", "COM1", "COM2",
+	"COM3", "COM4", "COM5", "COM6", "COM7", "COM8", "COM9", "LPT1",
+	"LPT2", "LPT3", "LPT4", "LPT5", "LPT6", "LPT7", "LPT8", "LPT9"};
+
+
+#endif /* DEFS_H */
