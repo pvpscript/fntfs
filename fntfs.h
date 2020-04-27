dfede1e35e2147cb68f2943087846e7c81b5baf8
Pedro Vitor Piassa <pvpiassa@gmail.com>
2020-04-27 19:01:50 -0300

    Syntax corrections

--- fntfs.h
+++ fntfs.h
@@ -3,10 +3,10 @@
 
 #define MAX_PATH 260
 
-char *r_chars = ['<', '>', ':', '"', '/', '\\', '|', '?', '*'];
-char *r_names = ["CON", "PRN", "AUX", "NUL", "COM1", "COM2", "COM3", "COM4",
+char r_chars[] = {'<', '>', ':', '"', '/', '\\', '|', '?', '*'};
+char *r_names[] = {"CON", "PRN", "AUX", "NUL", "COM1", "COM2", "COM3", "COM4",
      "COM5", "COM6", "COM7", "COM8", "COM9", "LPT1", "LPT2", "LPT3", "LPT4",
-     "LPT5", "LPT6", "LPT7", "LPT8", "LPT9"];
+     "LPT5", "LPT6", "LPT7", "LPT8", "LPT9"};
 
 
 /* Check wether the given path is a directory */
